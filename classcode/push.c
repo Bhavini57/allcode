28f90265de461f8c23c4c42aabff822405d40dfb
Bhavini <BHAVINI.22584@stu.upes.ac.in>
2025-12-25 00:19:05 +0530

    daily push

--- classcode/push.c
+++ classcode/push.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    printf("day 4 of push");
-    return 0; 
+ int a[10] = {10,20,30,40};
+ int n = 4;
+ int pos = 2;     // index where to insert
+ int value = 25;
+
+ for(int i = n; i > pos; i--)
+    a[i] = a[i-1];
+
+ a[pos] = value;
+ n++;
+ return 0; 
 }
